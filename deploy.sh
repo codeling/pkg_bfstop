84b9c4b2225998084b5cb72de0b830645537461f
Bernhard Froehler <bernhard@bfroehler.info>
2013-05-13 01:14:22 +0200

    Adds better description, removes non-working update URL, adds package files, deploy script

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+name=bfstop
+version=0.9.10
+
+pkg_name=pkg_${name}
+zipfile_name=${pkg_name}-${version}.zip
+src_files="${pkg_name}.xml packages"
+
+rm -f ${zipfile_name}
+zip -r ${zipfile_name} ${src_files}
+
